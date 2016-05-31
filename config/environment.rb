e8d7f9affbc70e225c50178205548c6863ac9c87
hieunm6 <hieunmce@gmail.com>
2016-05-31 10:26:50 +0700

    Add cors for post, path, delete request

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,16 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
+
+Rails.application.configure do
+	config.middleware.insert_before 0, "Rack::Cors" do
+		allow do
+			# In development, we don't care about the origin.
+			origins '*'
+			# Reminder: On the following line, the 'methods' refer to the 'Access-
+			# Control-Request-Method', not the normal Request Method.
+			resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :put, :patch], credentials: true
+		end
+	end
+end
 Rails.application.initialize!
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+	# This handles cross-origin resource sharing.
+	# See: https://github.com/cyu/rack-cors
 end
